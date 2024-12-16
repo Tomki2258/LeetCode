c6637a164d4d9feb9bc20a82dd30748c507d12a1
tamus <52979692+Tomki2258@users.noreply.github.com>
2024-12-16 21:53:50 +0100

    Time: 10 ms (100.00%) | Memory: 12.3 MB (22.32%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void insertionSort(vector<int>& arr, int n)
+{
+    for (int i = 1; i < n; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int inserted = 0;
+        for(int i = nums1.size() - 1 ;i >= 0;--i)
+        {
+            if(i > m - 1)
+            {
+                nums1[i] = nums2[n - inserted - 1];
+                inserted++;
+            }
+        }
+        insertionSort(nums1,m+n);
+        for(int i = 0;i < m+n;++i)
+        {
+            std::cout << nums1[i] << " ";
+        }
+    }
+};
