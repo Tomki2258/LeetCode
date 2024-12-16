a91a1d9211b2bb848427776f62b1e5bc074e7cda
tamus <52979692+Tomki2258@users.noreply.github.com>
2024-12-16 15:45:59 +0100

    Time: 3 ms (15.50%) | Memory: 9.3 MB (11.47%) - LeetSync

--- 290-word-pattern/word-pattern.cpp
+++ 290-word-pattern/word-pattern.cpp
@@ -2,64 +2,59 @@
     Implementacja struktóry imitującą mape
 */
 
-class Solution {
-public:
+class Solution
+{
+    public:
 
-    struct Element{
-        char key;
-        string word;
-    };
+        struct Element
+        {
+            char key;
+            string word;
+        };
 
-    bool wordPattern(string pattern, string s) {
-
+    bool wordPattern(string pattern, string s)
+    {
         vector<Element> elements;
 
-        stringstream ss (s);
+        stringstream ss(s);
         string word;
         short index = 0;
 
-    
         while (ss >> word)
         {
-            //Sprawdzenie czy dany element już zawiera się w vectorze
-            //Jesli klucz nie pasuje do słowa zwroc false
-            //A jak pasuje to nie dodawaj 
+           	//Sprawdzenie czy dany element już zawiera się w vectorze
+           	//Jesli klucz nie pasuje do słowa zwroc false
+           	//A jak pasuje to nie dodawaj 
 
-            for(int i = 0 ; i < elements.size();++i)
+            for (int i = 0; i < elements.size(); ++i)
             {
-                if(elements[i].key == pattern[index])
+                if (elements[i].key == pattern[index])
                 {
-                    if(elements[i].word != word)
+                    if (elements[i].word != word)
                     {
                         return false;
                     }
                 }
             }
 
-            //Dodaj sprawdzenie czy dla danego klucza istnieje jedno slowo 
-
-            for(int i = 0; i < elements.size();++i)
+            for (int i = 0; i < elements.size(); ++i)
             {
-                if(elements[i].word == word && elements[i].key != pattern[index]) return false;    
+                if (elements[i].word == word && elements[i].key != pattern[index]) return false;
             }
 
-            elements.push_back({pattern[index],word});
-
+            elements.push_back({ pattern[index],
+                word });
 
             index++;
         }
 
-
-        for(int i = 0 ;i < elements.size();++i)
-            {
-                std::cout << elements[i].key << " " << elements[i].word << "\n";
-            }   
-            std::cout << index << " " << pattern.size() << "\n";
-        if(index == pattern.size())
-        {   
-            return true;
+        for (int i = 0; i < elements.size(); ++i)
+        {
+            std::cout << elements[i].key << " " << elements[i].word << "\n";
         }
-        else{
+        std::cout << index << " " << pattern.size() << "\n";
+        if (index != pattern.size())
+        {
             return false;
         }
         return true;
