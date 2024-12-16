af7ef198898c3e54928303fee2ef59c9de260a7c
tamus <52979692+Tomki2258@users.noreply.github.com>
2024-12-16 15:34:11 +0100

    Time: 0 ms (100.00%) | Memory: 9.2 MB (11.47%) - LeetSync

---
+++ 290-word-pattern/word-pattern.cpp
@@ -0,0 +1,67 @@
+/*
+    Implementacja struktóry imitującą mape
+*/
+
+class Solution {
+public:
+
+    struct Element{
+        char key;
+        string word;
+    };
+
+    bool wordPattern(string pattern, string s) {
+
+        vector<Element> elements;
+
+        stringstream ss (s);
+        string word;
+        short index = 0;
+
+    
+        while (ss >> word)
+        {
+            //Sprawdzenie czy dany element już zawiera się w vectorze
+            //Jesli klucz nie pasuje do słowa zwroc false
+            //A jak pasuje to nie dodawaj 
+
+            for(int i = 0 ; i < elements.size();++i)
+            {
+                if(elements[i].key == pattern[index])
+                {
+                    if(elements[i].word != word)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            //Dodaj sprawdzenie czy dla danego klucza istnieje jedno slowo 
+
+            for(int i = 0; i < elements.size();++i)
+            {
+                if(elements[i].word == word && elements[i].key != pattern[index]) return false;    
+            }
+
+            elements.push_back({pattern[index],word});
+
+
+            index++;
+        }
+
+
+        for(int i = 0 ;i < elements.size();++i)
+            {
+                std::cout << elements[i].key << " " << elements[i].word << "\n";
+            }   
+            std::cout << index << " " << pattern.size() << "\n";
+        if(index == pattern.size())
+        {   
+            return true;
+        }
+        else{
+            return false;
+        }
+        return true;
+    }
+};
