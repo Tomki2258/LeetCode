a5886e7c577d08a32b5757f36179175ab04a6572
tamus <52979692+Tomki2258@users.noreply.github.com>
2024-12-28 20:52:02 +0000

    Time: 0 ms (100.00%) | Memory: 18.4 MB (5.43%) - LeetSync

---
+++ 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findSpecialInteger(vector<int>& arr) {
+        std::map<int,int> values;
+
+        for(int i = 0 ; i < arr.size();++i){
+
+            if (values.find(arr[i]) != values.end()) {
+                values[arr[i]]++;
+            }
+            values.insert({arr[i],1});
+        }
+
+        for (auto& i : values) {
+            float value = static_cast<float>(i.second) / arr.size(); 
+            if (value > 0.25f) {
+                return i.first;
+            }
+        }
+        return 0;
+    }
+};
