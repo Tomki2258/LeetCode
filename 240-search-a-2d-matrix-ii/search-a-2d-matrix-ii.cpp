aab4298c62b3a814fe55752cec6ba2a5d6c8329c
tamus <52979692+Tomki2258@users.noreply.github.com>
2024-12-29 14:07:11 +0000

    Time: 581 ms (8.95%) | Memory: 18.7 MB (20.66%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -5,11 +5,9 @@ public:
             if(matrix[i][matrix[i].size() / 2] == target) return true;
             for(int j = 0;j<matrix[i].size();++j)
             {
-                int val = matrix[i][j];
-                if(val == target) return true;
-                else if(val > target) break;
+                if(matrix[i][j] == target) return true;
+                else if(matrix[i][j] > target) break;
             }
-            std::cout << "\n";
         }
         return false;
     }
