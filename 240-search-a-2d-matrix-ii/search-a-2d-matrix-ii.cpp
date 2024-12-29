46e7c6d35ebb6eb5c2f462f2c834053cb557adc7
tamus <52979692+Tomki2258@users.noreply.github.com>
2024-12-29 14:01:15 +0000

    Time: 584 ms (8.92%) | Memory: 18.7 MB (37.59%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         for(int i = 0;i < matrix.size();++i){
+            if(matrix[i][matrix[i].size() / 2] == target) return true;
             for(int j = 0;j<matrix[i].size();++j)
             {
                 int val = matrix[i][j];
