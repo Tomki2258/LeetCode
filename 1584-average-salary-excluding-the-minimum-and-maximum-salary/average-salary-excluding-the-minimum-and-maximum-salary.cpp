1678eeedc9164bb2a46a286bd19e66b093d4888a
tamus <52979692+Tomki2258@users.noreply.github.com>
2025-01-03 23:09:04 +0000

    Time: 0 ms (100.00%) | Memory: 9.7 MB (26.51%) - LeetSync

--- 1584-average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.cpp
+++ 1584-average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     double average(vector<int>& salary) {
+        std::sort(salary.begin(),salary.end());
+
         double average = 0;
-        int max = *std::max_element(salary.begin(), salary.end());
-        int min = *std::min_element(salary.begin(), salary.end());
-        
-        for(int i = 0;i < salary.size();++i){
-            if(salary[i] != max && salary[i] != min){
-                std::cout << salary[i] << " ";
-                average += salary[i];
-            }
+        for (int i = 1;i < salary.size() - 1;++i){
+            average += salary[i];
         }
 
         return average / (salary.size() - 2);
