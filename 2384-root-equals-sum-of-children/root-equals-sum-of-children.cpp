72ca3423e64bcf28201450a9fc661a6bc20c35b4
tamus <52979692+Tomki2258@users.noreply.github.com>
2025-01-06 12:26:53 +0000

    Time: 0 ms (100.00%) | Memory: 15.2 MB (21.95%) - LeetSync

--- 2384-root-equals-sum-of-children/root-equals-sum-of-children.cpp
+++ 2384-root-equals-sum-of-children/root-equals-sum-of-children.cpp
@@ -1,20 +1,20 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    bool checkTree(TreeNode* root) {
-        int l = root->left->val;
-        int r = root->right->val;
-        if(l+r == root->val) return true;
-        return false;
-    }
+class Solution
+{
+    public:
+        bool checkTree(TreeNode *root)
+        {
+            if (root->right->val + root->left->val == root->val) return true;
+            return false;
+        }
 };
