fc6a9e65078d9fe638d09a6159f3eee3aaa2e9b0
tamus <52979692+Tomki2258@users.noreply.github.com>
2025-01-18 16:29:03 +0100

    Time: 0 ms (100.00%) | Memory: 25.6 MB (61.85%) - LeetSync

--- 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.cpp
+++ 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     double minimumAverage(vector<int>& nums) {
-        vector<double> averages;
+        double smallest = INT_MAX;
         std::sort(nums.begin(),nums.end());
         for(int i = 0;i < nums.size() / 2;++i){
-            averages.push_back((nums[i] + nums[nums.size() - 1- i]) / 2.0);
+            double result = (nums[i] + nums[nums.size() - 1- i]) / 2.0;
+            if(result < smallest) smallest = result;
         }
-        return *std::min_element(averages.begin(),averages.end());
+        return smallest;
     }
 };
