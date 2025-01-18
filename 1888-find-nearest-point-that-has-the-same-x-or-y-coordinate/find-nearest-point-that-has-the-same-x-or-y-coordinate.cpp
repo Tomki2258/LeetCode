eca1c1c4b894921d6583551c11e673925d579489
tamus <52979692+Tomki2258@users.noreply.github.com>
2025-01-18 16:39:20 +0100

    Time: 3 ms (21.03%) | Memory: 63.6 MB (59.81%) - LeetSync

--- 1888-find-nearest-point-that-has-the-same-x-or-y-coordinate/find-nearest-point-that-has-the-same-x-or-y-coordinate.cpp
+++ 1888-find-nearest-point-that-has-the-same-x-or-y-coordinate/find-nearest-point-that-has-the-same-x-or-y-coordinate.cpp
@@ -9,9 +9,7 @@ class Solution
             {
                 if (points[i][0] == x || points[i][1] == y)
                 {
-                    std::cout << points[i][0] << " " << points[i][1] << "\n";
                     int distance = abs(x - points[i][0]) + abs(y - points[i][1]);
-                    std::cout << distance << "\n";
                     if (distance < nearestDistance)
                     {
                         nearestIndex = i;
