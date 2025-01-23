533851d0f464769dc483665fa579726f21f0db99
tamus <52979692+Tomki2258@users.noreply.github.com>
2025-01-23 21:52:14 +0100

    Time: 0 ms (100.00%) | Memory: 12 MB (64.63%) - LeetSync

--- 682-baseball-game/baseball-game.cpp
+++ 682-baseball-game/baseball-game.cpp
@@ -1,23 +1,15 @@
 class Solution
 {
     public:
-        int isNumber(char sign)
-        {
-            return sign;
-        }
     int calPoints(vector<string> &operations)
     {
         vector<int> scores;
         for (int i = 0; i < operations.size(); ++i)
         {
-            std::cout << operations[i] << "\n";
             if(operations[i][0] == '+'){
-                int a = scores[scores.size() - 1];
-                int b = scores[scores.size() - 2];
-                scores.push_back(a+b);
+                scores.push_back(scores[scores.size() - 1]+scores[scores.size() - 2]);
             }else if(operations[i][0] == 'D'){
-                int last = scores[scores.size() - 1];
-                scores.push_back(last * 2);
+                scores.push_back(scores[scores.size() - 1] * 2);
             }else if(operations[i][0] == 'C'){
                 scores.pop_back();
             }
