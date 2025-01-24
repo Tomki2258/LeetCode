fa6f4e26a1c7d3dbdd02b8c2cba2f90c4f423421
tamus <52979692+Tomki2258@users.noreply.github.com>
2025-01-24 23:16:08 +0100

    Time: 0 ms (100.00%) | Memory: 12 MB (24.11%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -1,33 +1,26 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *    int val;
+ *    ListNode * next;
+ *    ListNode(int x) : val(x), next(NULL) {}
+ *};
  */
-class Solution {
-public:
-    vector<ListNode*> seen;
-    bool isIn(ListNode* ptr){
-        for(int i  =0 ;i  <seen.size();++i){
-            if(seen[i] == ptr){
-                return true;
+class Solution
+{
+    public:
+        bool hasCycle(ListNode *head)
+        {
+            if (head == nullptr || head->next == nullptr) return false;
+            ListNode *slow = head;
+            ListNode *fast = head->next;
+            while (fast != nullptr && fast->next != nullptr)
+            {
+                if (slow == fast) return true;
+                slow = slow->next;
+                fast = fast->next->next;
             }
-        }
-        return false;
-    }
-    bool hasCycle(ListNode *head) {
-        if(head == nullptr) return false;
-        ListNode* ptr = head;
-        while (ptr) {
-            if (isIn(ptr)) {
-                return true;
-            }
-            seen.push_back(ptr);
-            ptr = ptr->next;
-        }
 
-        return false;
-    }
+            return false;
+        }
 };
