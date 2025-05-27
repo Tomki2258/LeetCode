a50a631d82d482c162a434e109120ea0a60e23e5
tamus <52979692+Tomki2258@users.noreply.github.com>
2025-05-28 00:00:06 +0200

    Time: 90 ms (5.23%) | Memory: 48.4 MB (75.49%) - LeetSync

--- 1-two-sum/two-sum.cs
+++ 1-two-sum/two-sum.cs
@@ -1,7 +1,7 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
         for(int i = 0;i < nums.Length;i++){
-            for(int y = 0;y < nums.Length;y++){
+            for(int y = nums.Length - 1;y > 0;y--){
                 if(i == y) continue;
                 if(nums[i] + nums[y] == target){
                     return [i,y];
